2235e379277e92d858cf69f770f35f257d42a1a8
kvonflotow <vonflow@gmail.com>
2014-09-11 18:05:07 -0500

    new methods

--- getter.js
+++ getter.js
@@ -9,6 +9,7 @@
 		var LOGGING = true
 
 		// determine which matchesSelector to use once on load
+		// if else if else compiles smaller than a bunch of ifs
 		var MATCHES = ( function ( DOC_ELEMENT )
 			{
 				if ( DOC_ELEMENT.matches )
@@ -16,29 +17,34 @@
 					return 'matches'
 				}
 
-				if ( DOC_ELEMENT.matchesSelector )
+				else if ( DOC_ELEMENT.matchesSelector )
 				{
 					return 'matchesSelector'
 				}
 
-				if ( DOC_ELEMENT.webkitMatchesSelector )
+				else if ( DOC_ELEMENT.webkitMatchesSelector )
 				{
 					return 'webkitMatchesSelector'
 				}
 
-				if ( DOC_ELEMENT.mozMatchesSelector )
+				else if ( DOC_ELEMENT.mozMatchesSelector )
 				{
 					return 'mozMatchesSelector'
 				}
 
-				if ( DOC_ELEMENT.msMatchesSelector )
+				else if ( DOC_ELEMENT.msMatchesSelector )
 				{
 					return 'msMatchesSelector'
 				}
 
-				if ( DOC_ELEMENT.oMatchesSelector )
+				else if ( DOC_ELEMENT.oMatchesSelector )
 				{
 					return 'oMatchesSelector'
+				}
+
+				else
+				{
+					return false
 				}
 			}
 		)( DOC.documentElement );
@@ -73,6 +79,8 @@
 				// assume string
 				Getter_find.call( this, selector.toString() )
 			}
+
+			Getter_classes.call( this )
 		}
 
 		// enable/disable logging
@@ -103,7 +111,7 @@
 			},
 
 			{
-				regex: /^.[-A-Za-z0-9_:.]*$/,
+				regex: /^\.[-A-Za-z0-9_:.]*$/,
 
 				fn: function ( cls )
 				{
@@ -164,6 +172,58 @@
 			}
 		}
 
+		// add class methods to instance
+		// depends on classList being available, polyfill required for older browsers
+		function Getter_classes()
+		{
+			// reference this
+			var that = this
+
+			// define classList object
+			Object.defineProperty( that, 'classList',
+				{
+					// define methods in an object
+					value: {
+						add: function ( className )
+						{
+							for ( var i = 0, classes; i < that.length; ++i )
+							{
+								that[ i ].classList.add( className )
+							}
+						},
+
+						// only works on the first element in the instance
+						contains: function ( className )
+						{
+							if ( !that[ 0 ] )
+							{
+								return false // instance is empty
+							}
+
+							// not chainable, returns boolean
+							return that[ 0 ].classList.contains( className )
+						},
+
+						remove: function ( className )
+						{
+							for ( var i = 0, classes; i < that.length; ++i )
+							{
+								that[ i ].classList.remove( className )
+							}
+						},
+
+						toggle: function ( className )
+						{
+							for ( var i = 0, classes; i < that.length; ++i )
+							{
+								that[ i ].classList.toggle( className )
+							}
+						}
+					}
+				}
+			)
+		}
+
 		Getter.prototype.each = function ( fn )
 		{
 			for ( var i = 0; i < this.length; ++i )
@@ -173,6 +233,11 @@
 
 			// chainable
 			return this
+		}
+
+		Getter.prototype.eq = function ( index )
+		{
+
 		}
 
 		// executes the given method with provided arguments
@@ -205,18 +270,43 @@
 			}
 		}
 
+		// creates a new Getter instance
 		Getter.prototype.filter = function ( selector )
 		{
-			var that = this
-
-			var filtered = Array.prototype.filter.call( that, function ( element )
+			for ( var i = 0, filtered = []; i < this.length; ++i )
+			{
+				if ( this[ i ][ MATCHES ]( selector ) )
 				{
-					return element[ MATCHES ]( selector )
+					filtered.push( this[ i ] )
 				}
-			)
+			}
 
 			// return a new Getter object with filtered elements
 			return new Getter( filtered )
+		}
+
+		// attempt to return first element, if it doesn't exist, return this
+		// creates a new Getter instance if successful
+		// returns existing instance if unsuccessful
+		Getter.prototype.first = function ()
+		{
+			return this[ 0 ] ? new Getter( this[ 0 ] ) : this
+		}
+
+		// only operates on the first element in the instance
+		// does not create a new instance
+		// returns boolean
+		Getter.prototype.is = function ( selector )
+		{
+			return this[ 0 ] ? this[ 0 ][ MATCHES ]( selector ) : false
+		}
+
+		// attempt to return last element, if it doesn't exist, return this
+		// creates a new Getter instance if succesful
+		// returns existing instance if unsuccessful
+		Getter.prototype.last = function ()
+		{
+			return this[ 0 ] ? new Getter( this[ this.length - 1 ] ) : this
 		}
 
 		// provide convenience remove method
